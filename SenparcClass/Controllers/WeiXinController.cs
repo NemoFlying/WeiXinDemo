6f3b1cec6c80ebf94f9acccb01b03a24996c4e33
NemoFlying <895190626@qq.com>
2019-04-24 08:56:02 +0800

    添加项目文件。

---
+++ SenparcClass/Controllers/WeiXinController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.Entities.Request;
+using SenparcClassService;
+
+namespace SenparcClass.Controllers
+{
+    public class WeiXinController : Controller
+    {
+        private ILog log;
+        public WeiXinController(IHostingEnvironment hostingEnv)
+        {
+            this.log = LogManager.GetLogger(Startup.repository.Name, typeof(WeiXinController));
+        }
+
+        //public static readonly string Token = Config.SenparcWeixinSetting.Token;//与微信公众账号后台的Token设置保持一致，区分大小写。
+        public static readonly string Token = "Senparc20180909";
+
+        /// <summary>
+        /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：https://sdk.weixin.senparc.com/weixin
+        /// </summary>
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult Get(PostModel postModel, string echostr)
+        {
+            log.Debug("---!!!!!!!!!!!!!----");
+            if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, Token))
+            {
+                return Content(echostr); //返回随机字符串则表示验证通过
+            }
+            else
+            {
+                return Content("failed:" + postModel.Signature + "," + CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。" +
+                    "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致..。");
+            }
+        }
+
+        /// <summary>
+        /// 用户发送消息后，微信平台自动Post一个请求到这里，并等待响应XML。
+        /// PS：此方法为简化方法，效果与OldPost一致。
+        /// v0.8之后的版本可以结合Senparc.Weixin.MP.MvcExtension扩展包，使用WeixinResult，见MiniPost方法。
+        /// </summary>
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Post(PostModel postModel)
+        {
+            try
+            {
+                log.Debug("-------");
+                //创建MessageHandler MessageHandler
+                var messageHandler = new CustomMessageHandler(Request.Body, postModel);
+                messageHandler.SaveRequestMessageLog();
+                //执行
+                messageHandler.Execute();
+                messageHandler.SaveResponseMessageLog();
+                return Content(messageHandler.FinalResponseDocument.ToString());
+
+            }catch(Exception ex)
+            {
+                log.Error(ex);
+                return null;
+            }
+            
+
+        }
+
+
+
+
+
+
+        }
+}
---
+++ SenparcClassService/CustomMessageHandler.cs
@@ -0,0 +1,94 @@
+﻿using Senparc.NeuChar.Entities;
+using Senparc.NeuChar.Helpers;
+using Senparc.Weixin.MP.AppStore;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SenparcClassService
+{
+    public partial class CustomMessageHandler : MessageHandler<CustomMessageContext>
+    {
+        public CustomMessageHandler(Stream inputStream, PostModel postModel, int maxRecordCount = 0, DeveloperInfo developerInfo = null) : base(inputStream, postModel, maxRecordCount, developerInfo)
+        {
+        }
+
+        public CustomMessageHandler(XDocument requestDocument, PostModel postModel, int maxRecordCount = 0, DeveloperInfo developerInfo = null) : base(requestDocument, postModel, maxRecordCount, developerInfo)
+        {
+        }
+
+        public CustomMessageHandler(RequestMessageBase requestMessageBase, PostModel postModel, int maxRecordCount = 0, DeveloperInfo developerInfo = null) : base(requestMessageBase, postModel, maxRecordCount, developerInfo)
+        {
+        }
+
+        public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        {
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "当前服务器时间:" + DateTime.Now;
+            return responseMessage;
+        }
+
+        //文本消息
+        public override IResponseMessageBase OnTextRequest(RequestMessageText requestMessage)
+        {
+            //var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageText>();
+            //responseMessage.Content = $"你输入的是：{requestMessage.Content}";
+            //return responseMessage;
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageNews>();
+            var news = new Article
+            {
+                Title = $"知耻而后勇!",
+                Description = $"天下没有免费午餐\r\n更没有人亲自送到你手里！",
+                PicUrl = "https://b-ssl.duitang.com/uploads/item/201508/12/20150812193258_XfrHF.jpeg",
+                Url = "https://b-ssl.duitang.com/uploads/item/201508/12/20150812193258_XfrHF.jpeg"
+            };
+            responseMessage.Articles.Add(news);
+            return responseMessage;
+        }
+
+        //位置消息
+        public override IResponseMessageBase OnLocationRequest(RequestMessageLocation requestMessage)
+        {
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = $"发送的位置信息：Lat-{requestMessage.Location_X},Lon-{requestMessage.Location_Y}";
+            return responseMessage;
+            //return base.OnLocationRequest(requestMessage);
+        }
+
+        public override IResponseMessageBase OnEvent_ClickRequest(RequestMessageEvent_Click requestMessage)
+        {
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = $"你点击按钮：{requestMessage.EventKey}";
+            return responseMessage;
+            //return base.OnEvent_ClickRequest(requestMessage);
+        }
+
+        public override IResponseMessageBase OnEvent_SubscribeRequest(RequestMessageEvent_Subscribe requestMessage)
+        {
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageNews>();
+            var news = new Article
+            {
+                Title = $"欢迎加入微信开发!",
+                Description = $"天下没有免费午餐\r\n更没有人亲自送到你手里",
+                PicUrl = "https://b-ssl.duitang.com/uploads/item/201508/12/20150812193258_XfrHF.jpeg",
+                Url = "https://b-ssl.duitang.com/uploads/item/201508/12/20150812193258_XfrHF.jpeg"
+            };
+            responseMessage.Articles.Add(news);
+            return responseMessage;
+        }
+
+        public override void OnExecuted()
+        {
+            if(ResponseMessage is ResponseMessageText)
+            {
+                (ResponseMessage as ResponseMessageText).Content += "\r 【NEMO】";
+            }
+            base.OnExecuted();
+        }
+    }
+}
